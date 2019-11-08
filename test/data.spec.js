1bd0c9a04e342c7f741f7ee6f765b65dae3977de
Yanira-AB <arenazas2222@gmail.com>
2019-11-08 12:37:01 -0500

    test de searchPokemonByname

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,8 +1,19 @@
 /* eslint-disable linebreak-style */
-import { example } from '../src/data';
+import { example, searchPokemonByName } from '../src/data';
 
 describe('example', () => {
   it('debería ser una función', () => {
     expect(typeof example).toBe('function');
+  });
+});
+
+describe('searchPokemonByName', () => {
+  it('debería ser una función', () => {
+    expect(typeof searchPokemonByName).toBe('function');
+  });
+  it('debería retornar un obj con el nombre buscado', () => {
+    const data = [{ name: 'Bulbasaur' }, { name: 'Ivysaur' }, { name: 'Venusaur' }];
+    const result = [{ name: 'Ivysaur' }];
+    expect(searchPokemonByName(data, 'Ivysaur')).toStrictEqual(result);
   });
 });
